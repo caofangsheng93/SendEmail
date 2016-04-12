7749b58c8c73f27c86046a00477a3cef948a972b
caofangsheng93 <fangsheng93@163.com>
2016-04-12 11:53:10 +0800

    优化代码，提取公共方法

---
+++ SendEmail/Common/EmailHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+
+namespace SendEmail.Common
+{
+    public class EmailHelper
+    {
+        //发件人邮箱和密码
+        private static readonly string sendEmail = ConfigurationManager.AppSettings["SendEmail"];
+        private static readonly string sendEmailPWD = ConfigurationManager.AppSettings["SendEmailPWD"];
+
+        #region 发邮件
+        /// <summary>
+        /// 发邮件
+        /// </summary>
+        /// <param name="toEmail">收件人</param>
+        /// <param name="subject">主题</param>
+        /// <param name="body">内容主体</param>
+        /// <param name="cc">抄送给谁</param>
+        /// <param name="bcc">密送给谁</param>
+        /// <param name="isBodyHtml">是否有Body主体</param>
+        public static void SendEmail(string toEmail, string subject, string body, string cc, string bcc, bool isBodyHtml = true)
+        {
+            WebMail.SmtpServer = "smtp.163.com"; //gmail smtp server
+            WebMail.SmtpPort = 25; //gmail port to send emails
+            WebMail.SmtpUseDefaultCredentials = true;
+            WebMail.EnableSsl = true; //sending emails with secure protocol
+            WebMail.UserName = sendEmail;//EmailId used to send emails from application(发件人)
+            WebMail.Password = sendEmailPWD;//发件人密码（如果开通了授权码就要使用授权密码）
+            WebMail.From = sendEmail; //email sender email address.
+            WebMail.Send(to: toEmail, subject: subject, body: body, cc: cc, bcc: bcc, isBodyHtml: true);
+
+        }
+        #endregion
+    }
+}
--- SendEmail/Controllers/SendEmailController.cs
+++ SendEmail/Controllers/SendEmailController.cs
@@ -6,14 +6,13 @@ using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Configuration;
+using SendEmail.Common;
 
 namespace SendEmail.Controllers
 {
     public class SendEmailController : Controller
     {
-        //发件人邮箱和密码
-        private static readonly  string sendEmail = ConfigurationManager.AppSettings["SendEmail"];
-        private static readonly string sendEmailPWD = ConfigurationManager.AppSettings["SendEmailPWD"];
+        
         // GET: SendEmail
         public ActionResult Index()
         {
@@ -25,7 +24,8 @@ namespace SendEmail.Controllers
         {
             try
             {
-                SendEmail(model.ToEmail, model.EmailSubject, model.EMailBody, model.EmailCC, model.EmailBCC);
+                //发邮件
+               EmailHelper.SendEmail(model.ToEmail, model.EmailSubject, model.EMailBody, model.EmailCC, model.EmailBCC);
             }
             catch (Exception ex)
             {
@@ -36,28 +36,6 @@ namespace SendEmail.Controllers
             return View();
         }
 
-        #region 发邮件
-        /// <summary>
-        /// 发邮件
-        /// </summary>
-        /// <param name="toEmail">收件人</param>
-        /// <param name="subject">主题</param>
-        /// <param name="body">内容主体</param>
-        /// <param name="cc">抄送给谁</param>
-        /// <param name="bcc">密送给谁</param>
-        /// <param name="isBodyHtml">是否有Body主体</param>
-        public void SendEmail(string toEmail, string subject, string body, string cc, string bcc, bool isBodyHtml = true)
-        {
-            WebMail.SmtpServer = "smtp.163.com"; //gmail smtp server
-            WebMail.SmtpPort = 25; //gmail port to send emails
-            WebMail.SmtpUseDefaultCredentials = true;
-            WebMail.EnableSsl = true; //sending emails with secure protocol
-            WebMail.UserName = sendEmail;//EmailId used to send emails from application(发件人)
-            WebMail.Password = sendEmailPWD;//发件人密码（如果开通了授权码就要使用授权密码）
-            WebMail.From = sendEmail; //email sender email address.
-            WebMail.Send(to: toEmail, subject: subject, body: body, cc: cc, bcc: bcc, isBodyHtml: true);
-
-        } 
-        #endregion
+        
     }
 }
